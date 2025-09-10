06615008428d7c86950c0c100397b31138e9b3b4
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-09-10 16:12:41 +0200

    Added new route for every single msg

--- controller/messagesController.js
+++ controller/messagesController.js
@@ -1,10 +1,12 @@
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -15,13 +17,22 @@ async function getMessages(req, res) {
   res.render("index", { title: "Mini Message Board", messages });
 }
 
+async function openMessages(req, res) {
+  const id = Number(req.params.id);
+  const msg = messages.find((message) => message.id === id);
+
+  res.render("message", { title: "Message", msg });
+}
+
 async function formPage(req, res) {
   res.render("form");
 }
 async function newMessages(req, res) {
   const { msg, name } = req.body;
-  messages.push({ text: msg, user: name, added: new Date() });
+  const counter = messages.length + 1;
+
+  messages.push({ id: counter, text: msg, user: name, added: new Date() });
   res.redirect("/");
 }
 
-module.exports = { getMessages, formPage, newMessages };
+module.exports = { getMessages, openMessages, formPage, newMessages };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const indexRouter = Router();
 const {
   getMessages,
+  openMessages,
   formPage,
   newMessages,
 } = require("../controller/messagesController");
 
 indexRouter.get("/", getMessages);
+indexRouter.get("/messages/:id", openMessages);
 indexRouter.get("/new", formPage);
 indexRouter.post("/new", newMessages);
 
